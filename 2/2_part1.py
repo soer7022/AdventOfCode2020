255e041a48c38f202367295bfe81f61c70fd583e
Søren Eriksen <soer7033@gmail.com>
2020-12-02 09:23:32 +0100

    a tad cleaner code

--- 2/2_part1.py
+++ 2/2_part1.py
@@ -1,5 +1,3 @@
-from itertools import groupby
-
 data = []
 with open('input.txt') as f:
     data = [i.strip() for i in f.readlines()]
@@ -11,18 +9,7 @@ for line in data:
     constraints, password = line.split(": ")
     repetitions, letter = constraints.split(" ")
     min_reps, max_reps = [int(i) for i in repetitions.split("-")]
-    letter_map = dict()
-    if letter not in password:
-        invalid = True
-    if not invalid:
-        for l in password:
-            try:
-                letter_map[l] += 1
-            except KeyError:
-                letter_map[l] = 1
-        if not min_reps <= letter_map[letter] <= max_reps:
-            invalid = True
-    if not invalid:
+    if min_reps <= password.count(letter) <= max_reps:
         valid += 1
 
 print(valid)
--- 2/2_part2.py
+++ 2/2_part2.py
@@ -1,5 +1,3 @@
-from itertools import groupby
-
 data = []
 with open('input.txt') as f:
     data = [i.strip() for i in f.readlines()]
@@ -11,18 +9,14 @@ for line in data:
     constraints, password = line.split(": ")
     repetitions, letter = constraints.split(" ")
     min_reps, max_reps = [int(i) for i in repetitions.split("-")]
-    letter_map = dict()
-    if letter not in password:
-        invalid = True
-    if not invalid:
-        found_letter = False
-        if password[min_reps - 1] == letter:
-            found_letter = True
-        if password[max_reps - 1] == letter:
-            if found_letter:
-                invalid = True
-        elif not found_letter:
+    found_letter = False
+    if password[min_reps - 1] == letter:
+        found_letter = True
+    if password[max_reps - 1] == letter:
+        if found_letter:
             invalid = True
+    elif not found_letter:
+        invalid = True
 
     if not invalid:
         valid += 1
