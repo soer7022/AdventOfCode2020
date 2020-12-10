693b457c5c85ae570545b5af93b79528c8b8cf7a
Søren Eriksen <soer7033@gmail.com>
2020-12-10 11:55:09 +0100

    Small cleanup

--- 10/10_part1.py
+++ 10/10_part1.py
@@ -8,9 +8,7 @@ current_jolt = 0
 for adapter in data:
     diff = adapter - current_jolt
     current_jolt = adapter
-    if diff == 3:
-        diff_three += 1
-    elif diff == 1:
-        diff_one += 1
+    diff_three += diff == 3
+    diff_one += diff == 1
 
 print(diff_one*diff_three,diff_one,diff_three)
